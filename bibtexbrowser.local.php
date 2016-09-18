092e61be58fecd00d9ea4b779ef8df0c54ff9d64
k2rajara <krishan-25@hotmail.com>
2016-09-18 17:05:39 -0400

    First commit for jBibHtml.

---
+++ bibtexbrowser.local.php
@@ -0,0 +1,116 @@
+﻿<?php 
+/** displays the summary information of all bib entries.
+ * To be displayed in a Joomla article.
+ usage:
+ <pre>
+ $db = zetDB('metrics.bib');
+ $d = new ArticleDisplay();
+ $d->setDB($db);
+ $d->display();
+ </pre>
+ */
+class ArticleDisplay  {
+
+	var $headerCSS = 'sheader';
+
+	var $options = array();
+
+	var $headingLevel = BIBTEXBROWSER_HTMLHEADINGLEVEL;
+	function incHeadingLevel ($by=1) {
+		$this->headingLevel += $by;
+	}
+	function decHeadingLevel ($by=1) {
+		$this->headingLevel -= $by;
+	}
+
+	function setDB(&$bibdatabase) {
+		$this->setEntries($bibdatabase->bibdb);
+	}
+
+	function metadata() {
+		if (BIBTEXBROWSER_ROBOTS_NOINDEX) {
+			return array(array('robots','noindex'));
+		} else {
+			return array();
+		}
+	}
+
+	/** sets the entries to be shown */
+	function setEntries(&$entries) {
+		$this->entries = $entries;
+	}
+
+	function indexUp() {
+		$index=1;
+		foreach ($this->entries as $bib) {
+			$bib->setAbbrv((string)$index++);
+		} // end foreach
+		return $this->entries;
+	}
+
+	function newest(&$entries) {
+		return array_slice($entries,0,BIBTEXBROWSER_NEWEST);
+	}
+
+	function indexDown() {
+		$index=count($this->entries);
+		foreach ($this->entries as $bib) {
+			$bib->setAbbrv((string)$index--);
+		} // end foreach
+		return $this->entries;
+	}
+
+	function setQuery($query) {
+		$this->query = $query;
+	}
+	function getTitle() {
+		return _DefaultBibliographyTitle($this->query);
+	}
+
+	/** Displays a set of bibtex entries in an HTML table */
+	function display() {
+
+		uasort($this->entries, 'compare_bib_entries');
+
+		if ($this->options) {
+			foreach($this->options as $fname=>$opt) {
+				$this->$fname($opt,$entries);
+			}
+		}
+
+		if (BIBTEXBROWSER_DEBUG) {
+			echo 'Style: '.bibtexbrowser_configuration('BIBLIOGRAPHYSTYLE').'<br/>';
+			echo 'Order: '.ORDER_FUNCTION.'<br/>';
+			echo 'Abbrv: '.ABBRV_TYPE.'<br/>';
+			echo 'Options: '.@implode(',',$this->options).'<br/>';
+		}
+
+		if ($this->headingLevel == BIBTEXBROWSER_HTMLHEADINGLEVEL) {
+			echo "\n".'<span class="count">';
+			if (count($this->entries) == 1) {
+				echo count ($this->entries).' '.__('result');
+			} else if (count($this->entries) != 0) {
+				echo count ($this->entries).' '.__('results');
+			}
+			echo "</span>\n";
+		}
+		print_header_layout();
+
+		$count = count($this->entries);
+		$i=0;
+		$pred = NULL;
+		foreach ($this->entries as $bib) {
+			// by default, index are in decreasing order
+			// so that when you add a publicaton recent , the indices of preceding publications don't change
+			$bib->setIndex($count-($i++));
+			echo $bib->toHTML(true);
+
+			$pred = $bib;
+		} // end foreach
+
+		print_footer_layout();
+
+	} // end function
+
+} // end class
+?>
---
+++ jbibhtml.php
@@ -0,0 +1,130 @@
+﻿<?php
+/**
+ * @package    jbibhtml
+ * @subpackage com_jbibhtml
+ *
+ * @license  GNU General Public License version 2 or later
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+/**
+ * Jbibhtml Content Plugin
+ */
+class plgContentJbibhtml extends JPlugin
+{
+
+	/**
+	 * Method to parse $article->text string and insert bibliography html.
+	 *
+	 * Method is called by the view
+	 *
+	 * @param	string	The context of the content being passed to the plugin.
+	 * @param	object	The content object.  Note $article->text is also available
+	 * @param	object	The content params
+	 * @param	int		The 'page' number
+	 * 
+	 * @return  boolean	True on success.
+	 */
+	public function onContentPrepare($context, &$article, &$params, $limitstart)
+	{
+		//Do some quick checks to determine if we should process further.
+		//Possible contexts: com_content.featured, com_content.category, com_content.article
+		$parts = explode(".", $context);
+		
+		if ($parts[0] != 'com_content')
+		{
+			return true;
+		}
+		// Also add a quick check to see if we should process further.
+		//Note this is in complete in contexts like com_content.category where $article->text is the introtext.
+		if(JString::strpos($article->text, '{jbibhtml') === false) {
+			return true;
+		}
+		
+		$bibFile = $this->params->get('bibtex_file', '');
+		if(!preg_match("/{jbibhtml\s*(bibFile=\"([^\"]*)\")?\s*}/i", $article->text, $jbibhtml)) {
+			return true;
+		} elseif(count($jbibhtml) == 3) {
+			//Custom bibFile was given.
+			$bibFile = $jbibhtml[2];
+		}
+		
+		// Return if we don't have a bibtex_file.
+		if (empty($bibFile))
+		{
+			JError::raiseWarning( 100, "JBibHtml Error: Bibtex file not given." );
+			return true;
+		}
+		
+		//Load bibtexbrowser as a library
+		$_GET['library']=1;
+		define('BIBTEXBROWSER_BIBTEX_LINKS',false);
+		define('BIBTEXBROWSER_PDF_LINKS',false);
+		define('ABBRV_TYPE', $this->params->get('ABBRV_TYPE', 'index'));
+		require_once JPATH_SITE . '/plugins/content/jbibhtml/bibtexbrowser.php';
+		
+		global $db;
+		$db = new BibDataBase();
+		$db->load(JPATH_SITE . $bibFile);
+		
+		//Find all occurences like \cite{Smith2003} or \cite[theorem 5]{Smith2003} in $article->text.
+		$result_count = preg_match_all("/\\\cite(\[([^\]]*)\])?\{([^\}]*)\}/", $article->text, $pat_array);
+		
+		$resKeys = array();
+		//First loop through matches and fill in $resKeys.
+		for ($j = 0; $j < count($pat_array[0]); $j++) {
+			$match = $pat_array[3][$j];
+			$bibKeys = explode(",", $match);
+			for ($i = 0; $i < count($bibKeys); $i++) {
+				if ($db->contains($bibKeys[$i])) {
+					$resKeys[] = $bibKeys[$i];
+				}
+			}
+		}
+		
+		//We use our own custom method, ArticleDisplay. Could also use SimpleDisplay or AcademicDisplay.
+		//TODO: how are the entries being sorted?
+		$d = new ArticleDisplay();
+		//Heading level is decreased so that the ArticleDisplay->display function doesn't output total count.
+		$d->decHeadingLevel();
+		//Filter by bibtex keys, $resKeys$, cited in the article.
+		$entries = $db->multisearch(array('keys'=> $resKeys));
+		$d->setEntries($entries);
+		
+		//Load all HTML output into a string.
+		ob_start();
+		
+		$d->display();
+		$content = ob_get_contents();
+		ob_end_clean();
+		
+		$article->text = str_replace($jbibhtml[0], $content, $article->text);
+		
+		//In this loop, we create links such as [Smith2003] which replaces \cite{Smith2003} in $article->text.
+		//It also handles the case we have something like \cite[theorem 5]{Smith2003}.
+		for ($j = 0; $j < count($pat_array[0]); $j++) {
+			$match = $pat_array[3][$j];
+			$bibKeys = explode(",", $match);
+			$link = "[";
+			for ($i = 0; $i < count($bibKeys); $i++) {
+				if (!$db->contains($bibKeys[$i])) {
+					JError::raiseWarning( 100, "JBibHtml Error: Bibtex key '$bibKeys[$i]' not found in file '$bibFile'." );
+				} else {
+					$entry = $db->getEntryByKey($bibKeys[$i]);
+					if($i > 0) $link .= "; ";
+					$link .= '<a href="' . JURI::current(). '#' . $entry->getRawAbbrv() . '"><span class="bibkey">' . $entry->getRawAbbrv() . '</span></a>';
+				}
+			}
+			if($pat_array[2][$j] != "") {
+				$link .= ", " . $pat_array[2][$j];
+			}
+			$link .= "]";
+			//Link to the bibtex entry.
+			$article->text = str_replace($pat_array[0][$j], $link, $article->text);
+		}
+		
+		return true;
+	}
+}
